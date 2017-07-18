95b2783bc6d0f517e1e49ad93ff9bea82f2f90a8
Nagnath Dahale <nagnathdahale@gmail.com>
2017-07-18 18:12:44 +0530

    Add files via upload

---
+++ age_Visualization.py
@@ -0,0 +1,24 @@
+import pandas as plt
+import sqlite3
+import matplotlib.pyplot as pyp
+import matplotlib.pylab as plt
+from matplotlib import colors
+from datetime import datetime,date
+from dateutil import parser
+
+con = sqlite3.connect("database.sqlite")
+cur = con.cursor() 	 	
+age_set = {}
+today = date.today()
+for i in cur.execute('select player_name,birthday from player LIMIT 10'):
+    x =  str(i[0]).split(' ')
+    formatted_date = parser.parse(i[1])
+    age= today.year - formatted_date.year - ((today.month, today.day) < (formatted_date.month,formatted_date.day))
+    age_set[x[1]] = age
+pyp.bar(range(len(age_set)),age_set.values(),align='center',color='red')
+pyp.xticks(range(len(age_set)),age_set.keys(),rotation='vertical')
+pyp.xlabel("Player Name")
+pyp.ylabel("AGE");
+for a,b in zip(range(len(age_set)),age_set.values()):
+    plt.text(a, b, str(b))
+pyp.show()
